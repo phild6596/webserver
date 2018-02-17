9eaaf99871ac7e4ad843dc100092ae02edab3063
phild6596 <phild6596@gmail.com>
2018-02-17 11:17:42 -0800

    cleaned up code

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
-// Require/import the HTTP module
 var http = require("http");
 
-// Define a port to listen for incoming requests
+
 var PORT = 7000;
 var PORTTWO = 7500;
-
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
-
-    // Send the below string to the client when the user visits the PORT URL
-    response.end("You're AWESOME! Great job!");
-
-}
-
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
+var secondServer = http.createServer(secondRequest);
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function () {
-
-    // Log (server-side) when our server has started
-    console.log('Positive response');
-});
+function handleRequest(request, response) {
+    response.end("You're AWESOME! Great job!");
+}
 
 function secondRequest(request, response) {
     response.end('Today wasnt your day, go back to bed.');
 }
 
-var secondServer = http.createServer(secondRequest);
+server.listen(PORT, function () {
+    console.log('Positive response on Port: ' + PORT);
+});
 
 secondServer.listen(PORTTWO, function () {
-    console.log('Negative response');
+    console.log('Negative response on Port: ' + PORTTWO);
 })
+
+
+
